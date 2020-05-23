4fa670c9fb4f7c84003725d87c28f8cacb2e6762
Arjun Rao <arjun.rao@intersection.com>
2020-05-22 22:45:57 -0500

    First commit of Vue frontend app 🦄

---
+++ prr/vueapp/src/components/Haro.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <h3>HARO (Help A Reporter Out)</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Haro',
+    data() {
+      return {
+        users: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
