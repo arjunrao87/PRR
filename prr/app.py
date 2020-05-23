f7a4e483f32774198da6c226cd46bb20ecea653f
Arjun Rao <arjun.rao@intersection.com>
2020-05-22 21:49:54 -0500

    Fixing casing

--- prr/app.py
+++ prr/app.py
@@ -12,7 +12,7 @@ def home():
 @app.route('/parse-haro-email', methods = ['POST'])
 def parse_haro_email():
     email_id= request.form.get('emailId')
-    secret = args['secret']
+    secret = request.args['secret']
     try:
         if secret == os.getenv("PRR_SECRET"):
             return haro.convert_haro_email_to_records(email_id)
