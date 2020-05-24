d79420705a9c8f3e474bc750cb98c157aed4cf73
Arjun Rao <arjun.rao@intersection.com>
2020-05-24 13:06:09 -0500

    feat: vueapp now reads from Airtable

--- prr/vueapp/src/components/Haro.vue
+++ prr/vueapp/src/components/Haro.vue
@@ -4,18 +4,24 @@
     <table class="table">
       <thead>
         <tr>
-          <th scope="col">Id</th>
+          <th scope="col">Category</th>
+          <th scope="col">Summary</th>
           <th scope="col">Name</th>
+          <th scope="col">Media outlet</th>
           <th scope="col">Email</th>
-          <th scope="col">City</th>
+          <th scope="col">Deadline date</th>
+          <th scope="col">Deadline time</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="user in users" v-bind:key="user.id"> 
-          <th scope="row">{{user.id}}</th>
-          <td>{{user.name}}</td>
-          <td>{{user.email}}</td>
-          <td>{{user.address.city}}</td>
+        <tr v-for="story in stories" v-bind:key="story.id"> 
+          <td>{{story.get('category')}}</td>
+          <td>{{story.get('summary')}}</td>
+          <td>{{story.get('name')}}</td>
+          <td>{{story.get('media_outlet')}}</td>
+          <td>{{story.get('email')}}</td>
+          <td>{{story.get('deadline_date')}}</td>
+          <td>{{story.get('deadline_time')}}</td>
         </tr>
       </tbody>
     </table> 
@@ -23,23 +29,36 @@
 </template>
 
 <script>
-  import axios from 'axios';
-
-  export default {
-    name: 'Haro',
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: process.env.VUE_APP_AIRTABLE_API_KEY}).base(process.env.VUE_APP_AIRTABLE_PRR_BASE);
+export default {
+    name: "Haro",
     data() {
       return {
-        users: null,
+        stories: null
       };
     },
-    created: function() {
-      axios
-        .get('https://jsonplaceholder.typicode.com/users')
-        .then(res => {
-          this.users = res.data;
-        })
-    }
-  }
+    created: function(){
+      var vm = this;
+      let allStories=[]
+      base(process.env.VUE_APP_AIRTABLE_HARO).select({
+          maxRecords: 1000,
+          view: "Grid view"
+      }).eachPage(function page(records, fetchNextPage) {
+          console.log("In the page")
+          records.forEach(function(record) {
+          console.log("In the recordpush")
+            allStories.push(record);
+          });
+          fetchNextPage();
+        }, function done(err) {
+            if (err) { console.error(err); return; }
+          console.log("In the done")
+            vm.stories=allStories;
+            return allStories;
+        });
+    },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
